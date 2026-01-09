6cccebc795cdad1842b26724d4bf3eb33482461f
Administrator <Administrator@U6PRN00DL-06>
2026-01-09 18:11:56 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReadQR = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(21, 12);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(750, 377);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(750, 377);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(122, 428);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(498, 428);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnReadQR);
+            this.panel1.Controls.Add(this.hWindowControl1);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 554);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnReadQR
+            // 
+            this.btnReadQR.Location = new System.Drawing.Point(322, 428);
+            this.btnReadQR.Name = "btnReadQR";
+            this.btnReadQR.Size = new System.Drawing.Size(75, 23);
+            this.btnReadQR.TabIndex = 3;
+            this.btnReadQR.Text = "QR";
+            this.btnReadQR.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReadQR;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private HTuple _acqHandle = null;
+        private CancellationTokenSource _cts;
+        private volatile bool _running = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += (s, e) => StopCam();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (_running) return;
+
+            try
+            {
+                // 1) Open camera (tham số giống y hệt open_framegrabber bạn copy)
+                HOperatorSet.OpenFramegrabber(
+                    "GigEVision2",
+                    0, 0, 0, 0, 0, 0,
+                    "progressive",   // <--- như HDevelop
+                    -1,
+                    "default",
+                    -1,
+                    "false",
+                    "default",
+                    "Camera1",
+                    0,
+                    -1,
+                    out _acqHandle
+                );
+
+                // 2) Start grabbing
+                HOperatorSet.GrabImageStart(_acqHandle, -1);
+
+                _cts = new CancellationTokenSource();
+                _running = true;
+                Task.Run(() => GrabLoop(_cts.Token));
+            }
+            catch (Exception ex)
+            {
+                _running = false;
+                MessageBox.Show("Start lỗi: " + ex.Message);
+            }
+        }
+
+        private void GrabLoop(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                HObject img = null;
+                try
+                {
+                    HOperatorSet.GrabImageAsync(out img, _acqHandle, -1);
+
+                    BeginInvoke(new Action(() =>
+                    {
+                        try
+                        {
+                            hWindowControl1.HalconWindow.DispObj(img);
+                        }
+                        finally
+                        {
+                            img?.Dispose();
+                        }
+                    }));
+                }
+                catch
+                {
+                    img?.Dispose();
+                    Thread.Sleep(5);
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopCam();
+        }
+
+        private void StopCam()
+        {
+            try
+            {
+                _cts?.Cancel();
+                _cts = null;
+
+                if (_acqHandle != null)
+                {
+                    try { HOperatorSet.CloseFramegrabber(_acqHandle); } catch { }
+                    _acqHandle = null;
+                }
+            }
+            finally
+            {
+                _running = false;
+            }
+        }
+    }
+}
